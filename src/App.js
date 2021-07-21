b5b423fdf4c58bb6bb0b51f37c6ddf9a4d0a2cec
Yuliia Bondarenko <bondarenko047@gmail.com>
2021-07-21 12:10:41 +0300

    + localStorage

--- src/App.js
+++ src/App.js
@@ -53,6 +53,23 @@ class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parselContacts = JSON.parse(contacts);
+
+    if (parselContacts) {
+      this.setState({ contacts: parselContacts });
+    };
+    
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
